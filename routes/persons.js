6ce752356c9864262617679dd2f2bb8fea9d527a
OgutuJoseph <onyango.joseph@yahoo.com>
2022-09-07 12:28:21 +0300

    Greater than / less than operators

--- routes/persons.js
+++ routes/persons.js
@@ -1,12 +1,14 @@
 import express from 'express'; 
-import { findMany, countAll, equalityQuery } from '../controller/person.js';
+import { findMany, countAll, equalityQuery, notEqualTo, greaterThan } from '../controller/person.js';
 
 const router = express.Router();
 
 /** Persons routes */
 //skipped insert Many
-router.get('/findMany', findMany) 
-router.get('/count', countAll)
-router.get('/equalityQuery', equalityQuery)
+router.get('/findMany', findMany);
+router.get('/count', countAll);
+router.get('/equalityQuery', equalityQuery);
+router.get('/notEqualTo', notEqualTo);
+router.get('/greaterThan', greaterThan);
 
 export default router;
