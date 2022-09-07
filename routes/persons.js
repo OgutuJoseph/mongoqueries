d8e763fba3d14290ad34bee459fa6121130b6236
OgutuJoseph <onyango.joseph@yahoo.com>
2022-09-07 14:47:13 +0300

    Working with array operators,  and

--- routes/persons.js
+++ routes/persons.js
@@ -1,5 +1,17 @@
 import express from 'express'; 
-import { findMany, countAll, equalityQuery, notEqualTo, greaterThan, inAndNotIn, andOperator, orOperator, nestedDocuments, arrayInDocuments } from '../controller/person.js';
+import { 
+    findMany, 
+    countAll, 
+    equalityQuery, 
+    notEqualTo, 
+    greaterThan, 
+    inAndNotIn, 
+    andOperator, 
+    orOperator, 
+    nestedDocuments, 
+    arrayInDocuments, 
+    arrayOperators 
+} from '../controller/person.js';
 
 const router = express.Router();
 
@@ -14,6 +26,7 @@ router.get('/inAndNotIn', inAndNotIn);
 router.get('/andOperator', andOperator);
 router.get('/orOperator', orOperator);
 router.get('/nestedDocuments', nestedDocuments);
-router.get('/arrayInDocuments', arrayInDocuments)
+router.get('/arrayInDocuments', arrayInDocuments);
+router.get('/arrayOperators', arrayOperators);
 
 export default router;
